6bb7510a870ee993d53e99a341de4dde8b85e218
Peter Lesslie <pclesslie@gmail.com>
2020-01-16 16:52:01 -0600

    clean

--- first-missing-positive/missing_positive.cpp
+++ first-missing-positive/missing_positive.cpp
@@ -12,42 +12,8 @@ using namespace std;
 
 struct Solution
 {
-    static void printarr(vector<int>& nums) noexcept
-    {
-        for (auto n : nums) printf("%d ", n);
-        printf("\n");
-    }
     static int firstMissingPositive(vector<int>& nums) noexcept
     {
-        // // O(n) + O(n*log(n)) => O(n*log(n)) solution
-        // if (std::find(nums.begin(), nums.end(), 1) == nums.end()) return 1;
-        // std::sort(nums.begin(), nums.end());
-        // int i, n;
-        // for (i = 0, n = nums.size(); i < n - 1; ++i) {
-        //     if (nums[i] <= 0)             continue;
-        //     if (nums[i] == nums[i+1])     continue;
-        //     if (nums[i] == nums[i+1] - 1) continue;
-        //     break;
-        // }
-        // return nums[i] + 1;
-
-
-        // // using O(n) space, O(2n) complexity:
-        // int N = nums.size();
-        // std::vector<bool> present(N+1, false);
-        // for (int i = 0; i < N; ++i) {
-        //     if (1 <= nums[i] && nums[i] <= N)
-        //         present[nums[i] - 1] = true;
-        // }
-
-        // for (int i = 0; i < N+1; ++i) {
-        //     if (!present[i])
-        //         return i + 1;
-        // }
-
-        // __builtin_unreachable();
-        // return -1;
-
         int N = nums.size();
         for (int i = 0; i < N; ++i) {
             for (;;) {
