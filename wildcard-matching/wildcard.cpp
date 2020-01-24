31e3e42149dc63e1fd732264e6f76d5f13e59646
Peter Lesslie <pclesslie@gmail.com>
2020-01-24 11:27:28 -0600

    Wildcard matching using DP

---
+++ wildcard-matching/wildcard.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+#include "json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+struct Solution
+{
+    static bool isMatch(string text, string pat) noexcept
+    {
+        int N = text.size() + 1;
+        int M = pat.size() + 1;
+        vector<vector<bool>> A;
+        for (int i = 0; i < N; ++i)
+            A.push_back(vector<bool>(M, false));
+        A[0][0] = true;
+        for (int j = 1; j < M; ++j)
+            if (pat[j-1] == '*')
+                A[0][j] = A[0][j-1];
+        for (int i = 1; i < N; ++i) {
+            for (int j = 1; j < M; ++j) {
+                if (pat[j-1] == '?' || pat[j-1] == text[i-1])
+                    A[i][j] = A[i-1][j-1];
+                else if (pat[j-1] == '*')
+                    A[i][j] = A[i-1][j] || A[i][j-1];
+            }
+        }
+        return A[N-1][M-1];
+    }
+
+    // static bool matchstar(const char* pat, const char* str) noexcept
+    // {
+    //     while (pat[0] == '*')
+    //         ++pat;
+    //     do {
+    //         if (match(pat, str))
+    //             return true;
+    //     } while (*str++ != '\0');
+    //     return false;
+    // }
+
+    // static bool match(const char* pat, const char* str) noexcept
+    // {
+    //     if (pat[0] == '\0')
+    //         return str[0] == '\0';
+    //     else if (pat[0] == '*')
+    //         return matchstar(pat + 1, str);
+    //     else if (str[0] != '\0' && (pat[0] == '?' || pat[0] == str[0]))
+    //         return match(pat + 1, str + 1);
+    //     return false;
+    // }
+
+    // static bool isMatch(string s, string p) noexcept
+    // {
+    //     return match(p.c_str(), s.c_str());
+    // }
+};
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cerr << "usage: " << argv[0] << " [FILE]" << std::endl;
+        return 0;
+    }
+
+    std::ifstream ifs(argv[1]);
+    if (!ifs) {
+        std::cerr << "error: unable to open input file: " << argv[1] << std::endl;
+        return 1;
+    }
+
+#if 1
+    vector<tuple<string, string, bool>> cases = {
+        // string   , pattern   , expect
+        { "aa"       , "aa"     , true  },
+        { "aa"       , "a"      , false },
+        { "aa"       , "*"      , true  },
+        { ""         , "*"      , true  },
+        { ""         , "?"      , false },
+        { "aaaa"     , "b*"     , false },
+        { "adceb"    , "*a*b"   , true  },
+        { "adcebe"   , "*a*b"   , false },
+        { "acdcb"    , "a*c?b"  , false },
+        { "cb"       , "?a"     , false },
+        { ""         , "*?"     , false },
+        { "a"        , "*?"     , true  },
+        { "aa"       , "*?"     , true  },
+        { "ho"       , "ho**"   , true  },
+        { "ho"       , "ho***"  , true  },
+        { "hoh"      , "ho***h" , true  },
+        { "hoj"      , "ho***h" , false },
+    };
+
+    for (auto&& c : cases) {
+        auto str = std::get<0>(c);
+        auto pat = std::get<1>(c);
+        auto exp = std::get<2>(c);
+        bool res = Solution{}.isMatch(str, pat);
+        if (res != exp) {
+            printf("\n\n!!!! FAILED !!!!\n");
+            printf("string  = '%s'\n", str.c_str());
+            printf("pattern = '%s'\n", pat.c_str());
+            printf("result  = %s\n", (res ? "true" : "false"));
+            printf("expect  = %s\n", (exp ? "true" : "false"));
+            printf("\n");
+        }
+        // assert(res == exp);
+    }
+#endif
+
+    json j;
+    ifs >> j;
+    auto s = j.get<string>();
+    ifs >> j;
+    auto p = j.get<string>();
+
+    auto sol = Solution{}.isMatch(s, p);
+    std::cout << (sol ? "true" : "false") << std::endl;
+
+    return 0;
+}
